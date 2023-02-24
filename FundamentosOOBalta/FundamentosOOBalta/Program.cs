1b21eb8d2b4eefc84473b336365173aaa8b024fb
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2023-02-24 11:13:23 -0300

    initial

---
+++ FundamentosOOBalta/FundamentosOOBalta/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FundamentosOOBalta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Propriedades, eventos e métodos
+            //Um objeto sempre é um tipo de referência
+            // -> Endereço dos dados
+
+            Console.WriteLine("Hello!");
+
+            var pagamento = new Pagamento();
+        }
+    }
+
+    //Private, Protected, Internal e Public
+    class Pagamento
+    {
+        public virtual void Pagar(string numero) { }
+    }
+    class PagamentoCartaoCredito : Pagamento
+    {
+        public override void Pagar(string numero) { }
+    }
+}
