d26ace43cabd763816c4ef3a1ec282ea79b36091
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2023-05-26 12:37:04 -0300

    addd

---
+++ SistemaDeTarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BuscarPorId(int Id);
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int Id);
+        Task<bool> Apagar(int Id);
+    }
+}
