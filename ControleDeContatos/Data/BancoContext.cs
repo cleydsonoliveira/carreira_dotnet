5069b079f7c82417799063ee4a72bea8b1a179e6
cleydsonoliveira <cleydsonoliveiraips392@gmail.com>
2023-05-28 21:07:09 -0300

    ad

--- ControleDeContatos/Data/BancoContext.cs
+++ ControleDeContatos/Data/BancoContext.cs
@@ -1,5 +1,6 @@
 ﻿using ControleDeContatos.Models;
 using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace ControleDeContatos.Data
 {
@@ -10,5 +11,6 @@ namespace ControleDeContatos.Data
         }
 
         public DbSet<ContatoModel> Contatos { get; set; }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
     }
 }
